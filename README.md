73690bc81382c9b4ad3cee35ee32104dd784b577
drebob <99826856+drebob@users.noreply.github.com>
2023-10-11 14:23:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-andre-website
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/drebob/stackblitz-andre-website)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+import './style.css';
+
+export const App: FC<{ name: string }> = ({ name }) => {
+  return (
+    <div>
+      <h1>Hello you!</h1>
+      <p>This is the first version of my website :)</p>
+    </div>
+  );
+};
