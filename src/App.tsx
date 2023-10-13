52b0499936946554b2c340eb68d8a3fbbb4099e2
drebob <andrehallrodrigues@gmail.com>
2023-10-12 20:57:17 -0700

    added components folder and Projects.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
+import { Projects } from './components/Projects';
 
 import './style.css';
 
 export const App: FC<{ name: string }> = ({ name }) => {
   return (
     <div>
-      <h1>Hello you!</h1>
+      <h1>Hello, {name}!</h1>
       <p>This is the first version of my website :) whaoooooo </p>
+      <Projects />
     </div>
   );
 };
---
+++ src/components/Projects.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export const Projects: React.FC = () => (
+  <section id="projects">
+    <h2>My Projects</h2>
+    <div>
+      <h3>Project Title 1</h3>
+      <p>Description of the project. This was a really fun challenge...</p>
+      <a href="https://github.com/yourusername/project1">View on GitHub</a>
+    </div>
+    {/* Add more project divs as needed */}
+  </section>
+);
