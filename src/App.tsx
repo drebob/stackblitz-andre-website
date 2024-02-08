6a16522d15a47bf3597b587576f437dab212f63d
drebob <andrehallrodrigues@gmail.com>
2024-02-07 19:21:13 -0700

    tsparticles background working

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ import { Experience, About, Title, SocialMedia, Nav } from './components';
 export const App: FC = () => {
   return (
         <div className="relative bg-black-gradient mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
+          <Background />
           <div className="relative z-10">
             <div className={`lg:flex lg:justify-between lg:gap-4`}>
               <div className={`lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:justify-between lg:flex-col lg:py-24`}>
+                
                 <div>
-                  <Background />
                   <Title />
                   <Nav />
                 </div>
