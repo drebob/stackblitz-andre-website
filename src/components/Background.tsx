d8184a8432e0d96ff32f0d6bc59c72585da111e7
drebob <andrehallrodrigues@gmail.com>
2024-02-07 18:25:28 -0700

    Finally got tsparticles to display in background as a component

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,38 +1,56 @@
-import * as React from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
 
 const Background: React.FC = () => {
-  const particlesInit = async (main) => {
-    await loadFull(main);
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // Initialization logic
+      await loadSlim(engine);
+    }).then(() => setInit(true));
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
   };
 
-  const particlesLoaded = (container) => {
-    return Promise.resolve();
-  };
+  const options: ISourceOptions = useMemo(() => ({
+    // Particles options
+    background: { color: { value: "#0d47a1" } },
+    fpsLimit: 120,
+    interactivity: {
+      events: { onClick: { enable: true, mode: "push" }, onHover: { enable: true, mode: "repulse" } },
+      modes: { push: { quantity: 4 }, repulse: { distance: 200, duration: 0.4 } },
+    },
+    particles: {
+      color: { value: "#ffffff" },
+      links: { color: "#ffffff", distance: 150, enable: true, opacity: 0.5, width: 1 },
+      number: { density: { enable: true }, value: 80 },
+      opacity: { value: 0.5 },
+      shape: { type: "circle" },
+      size: { value: { min: 1, max: 5 } },
+    },
+    detectRetina: true,
+  }), []);
 
-  return (
-    <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        particles: {
-          number: {
-            value: 50,
-          },
-          links: {
-            enable: true,
-            opacity: 0.5,
-          },
-          move: {
-            enable: true,
-          },
-        },
-      }}
-      
-    />
-  );
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return null; // It's often more conventional to return null in React for conditional rendering
 };
 
 export default Background;
