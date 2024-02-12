44949d898fc800404a1994a85581b8e09f4531e2
drebob <andrehallrodrigues@gmail.com>
2024-02-12 02:18:58 -0700

    added some leading to title and fixed responsive conditionals

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -7,17 +7,17 @@ const Title = () => {
         
 
         <div className="flex flex-row justify-between items-center w-full">
-          <p className={'flex-1 font-poppins font-semibold xs:text-[72px] md:text-[40px] text-white'}>
+          <p className={'flex-1 font-poppins font-semibold xs:text-[52px] xs:leading-[20px] ss:text-[42px] sm:text-[42px] md:text-[50px] lg:text-[50px] xl:text-[50px] xxl:text-[50px] text-white'}>
             
             
-            Andre Hall-Rodrigues 
+            Andre <br className={'sm:hidden'}/>Hall-Rodrigues 
           </p>
           
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[72px] md:text-[40px] lg:text-[32px] text-white w-full text-gradient">
+        <p className={'font-poppins font-semibold xs:text-[38px] md:text-[50px] lg:text-[32px] text-white w-full'}>
           Web Developer
-        </h1>
+        </p>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Expert in digital design with Adobe Suite and AI, along with modern front-end technologies like Tailwind, React, and jQuery.
         </p>
