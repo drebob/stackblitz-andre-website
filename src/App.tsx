e727914b095eb769cd185300b0e18e12841c217b
drebob <andrehallrodrigues@gmail.com>
2024-02-21 15:58:36 -0700

    fixed background section nav

--- src/App.tsx
+++ src/App.tsx
@@ -260,7 +260,7 @@ export const App: FC = () => {
           <div className={'lg:w-1/2 lg:pb-24 xl:mb-9"'}>
            <About />
            <Experience />
-           <section id="backgrounds" className={'flex flex-col pt-24'}>
+           <section id="background" className={'flex flex-col pt-24'}>
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:px-0 lg:py-0 lg:opacity-0">
     <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
       Backgrounds
