1a0c441f36bc5a516a5f8ef11e85a5d0c1d72cc9
drebob <andrehallrodrigues@gmail.com>
2024-02-21 15:22:32 -0700

    fixed background and experience sections repsonive

--- src/App.tsx
+++ src/App.tsx
@@ -260,8 +260,8 @@ export const App: FC = () => {
           <div className={'lg:w-1/2 lg:pb-24 xl:mb-9"'}>
            <About />
            <Experience />
-           <section id="background" className="mt-16 lg:hover:bg-slate-800/50 p-4 rounded w-full flex-wrap" aria-label="Backgrounds">
-           <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+           <section id="backgrounds" className={'flex flex-col pt-24'}>
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:px-0 lg:py-0 lg:opacity-0">
     <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
       Backgrounds
     </h2>
