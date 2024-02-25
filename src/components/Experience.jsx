326d404da892ee0bdb44ef5b482a965d436093dc
drebob <andrehallrodrigues@gmail.com>
2024-02-25 04:26:12 -0700

    trying to fix resume link

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -505,7 +505,7 @@ const Experience = () => (
     <div className="mt-12">
       <a
         className="block m-auto items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-center"
-        href="/resume.pdf"
+        href="AndreHREsum222e.pdf"
         target="_blank"
         rel="noreferrer noopener"
         aria-label="View Full Résumé (opens in a new tab)"
